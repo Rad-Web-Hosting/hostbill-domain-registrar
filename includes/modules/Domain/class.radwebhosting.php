276bdd47cb3be35ff83ca59e8c91a14e35010f32
Rad Web Hosting <admin@radwebhosting.com>
2018-04-17 01:27:54 -0700

    Update class.radwebhosting.php

--- includes/modules/Domain/class.radwebhosting.php
+++ includes/modules/Domain/class.radwebhosting.php
@@ -1,33 +1,8 @@
 <?php
 
-/* * ********************************************************************
- * Customization Services by ModulesGarden.com
- * Copyright (c) ModulesGarden, INBS Group Brand, All Rights Reserved 
- * (2014-02-14, 11:50:55)
- * 
- *
- *  CREATED BY MODULESGARDEN       ->        http://modulesgarden.com
- *  CONTACT                        ->       contact@modulesgarden.com
- *
- *
- *
- *
- * This software is furnished under a license and may be used and copied
- * only  in  accordance  with  the  terms  of such  license and with the
- * inclusion of the above copyright notice.  This software  or any other
- * copies thereof may not be provided or otherwise made available to any
- * other person.  No title to and  ownership of the  software is  hereby
- * transferred.
- *
- *
- * ******************************************************************** */
 use domainsReseller\module\PdoWrapper;
 
 include_once dirname(__FILE__) . DS . 'PdoWrapper.php';
-
-/**
- * @author Grzegorz Draganik <grzegorz@modulesgarden.com>
- */
 
 class radwebhosting extends DomainModule {
 	
