3b52157571b4c11fb89cf5a6f3d95ebf2023ce6a
Rad Web Hosting <admin@radwebhosting.com>
2018-04-17 01:29:35 -0700

    Update PdoWrapper.php

--- modules/registrars/radwebhosting/PdoWrapper.php
+++ modules/registrars/radwebhosting/PdoWrapper.php
@@ -1,32 +1,8 @@
 <?php
 
-/* * ********************************************************************
- * DiscountCenter product developed. (2016-10-12)
- * *
- *
- *  CREATED BY MODULESGARDEN       ->       http://modulesgarden.com
- *  CONTACT                        ->       contact@modulesgarden.com
- *
- *
- * This software is furnished under a license and may be used and copied
- * only  in  accordance  with  the  terms  of such  license and with the
- * inclusion of the above copyright notice.  This software  or any other
- * copies thereof may not be provided or otherwise made available to any
- * other person.  No title to and  ownership of the  software is  hereby
- * transferred.
- *
- *
- * ******************************************************************** */
-
 namespace domainsReseller\module;
 use Illuminate\Database\Capsule\Manager as DB;
 
-/**
- * Description of PdoWrapper
- *
- * @author Pawel Kopec <pawelk@modulesgarden.com>
- * @version 1.0.0
- */
 class PdoWrapper
 {
     public static function query($query, $params = array())
