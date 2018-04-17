21456d8789c2de88db053cd821c95231770c594c
Rad Web Hosting <admin@radwebhosting.com>
2018-04-17 01:30:21 -0700

    Update class.api.php

--- modules/registrars/radwebhosting/class.api.php
+++ modules/registrars/radwebhosting/class.api.php
@@ -1,31 +1,5 @@
 <?php
 
-/* * ********************************************************************
- * Customization Services by ModulesGarden.com
- * Copyright (c) ModulesGarden, INBS Group Brand, All Rights Reserved 
- * (2013-09-23, 13:19:25)
- * 
- *
- *  CREATED BY MODULESGARDEN       ->        http://modulesgarden.com
- *  CONTACT                        ->       contact@modulesgarden.com
- *
- *
- *
- *
- * This software is furnished under a license and may be used and copied
- * only  in  accordance  with  the  terms  of such  license and with the
- * inclusion of the above copyright notice.  This software  or any other
- * copies thereof may not be provided or otherwise made available to any
- * other person.  No title to and  ownership of the  software is  hereby
- * transferred.
- *
- *
- * ******************************************************************** */
-
-/**
- * @author Grzegorz Draganik <grzegorz@modulesgarden.com>
- */
-
 if (!class_exists('radwebhosting_API')) {
 
     class radwebhosting_API {
